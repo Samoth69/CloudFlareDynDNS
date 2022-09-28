c571bdd986f3da7c16eb797164d28daa9fef4058
Thomas VIOLENT <thomasviolent@gmail.com>
2022-09-28 11:36:57 +0200

    DI and conf logging

---
+++ Config.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO.Enumeration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudflareDynDNS
+{
+    public class Config
+    {
+        private static ILogger _logger;
+
+        public Config(ILogger<Config> log)
+        {
+            _logger = log;
+        }
+
+        public ConfigTemplate GetConfigTemplate(string filename)
+        {
+            if (File.Exists(filename))
+            {
+
+            }
+            else
+            {
+                _logger.LogCritical("File {filename} not found, please fill it before restarting the app", filename);
+            }
+            return null;
+        }
+
+        public class ConfigTemplate
+        {
+            public string DiscordWebhookAddress { get; set; }
+            public string CloudflareToken { get; set; }
+            /// <summary>
+            /// Domain name that we need to find on cloudflare backend
+            /// </summary>
+            public string DomainName { get; set; }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,27 @@
-﻿namespace CloudflareDynDNS
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace CloudflareDynDNS
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var serviceProvider = new ServiceCollection()
+                .AddLogging(x => x.AddSimpleConsole(options =>
+                {
+                    options.IncludeScopes = true;
+                    options.SingleLine = true;
+                    options.TimestampFormat = "yyyy/MM/dd hh:mm:ss ";
+                }).SetMinimumLevel(LogLevel.Debug))
+                .AddSingleton<Config>()
+                .BuildServiceProvider();
+
+            var log = serviceProvider.GetService<ILoggerFactory>().CreateLogger<Program>();
+
+            var conf = serviceProvider.GetService<Config>();
+            var test = conf.GetConfigTemplate("conf.json");
+            log.LogInformation(conf.ToString());
         }
     }
 }
